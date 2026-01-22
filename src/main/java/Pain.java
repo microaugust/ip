b06ff471b102012555972c93804919f21b6695c9
microaugust <203528273+microaugust@users.noreply.github.com>
2026-01-22 12:34:45 +0800

    Level 1

--- src/main/java/Pain.java
+++ src/main/java/Pain.java
@@ -1,15 +1,26 @@
+import java.util.Scanner;
+
 public class Pain {
 
     private static void printLine() {
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
     }
 
     public static void main(String[] args) {
         printLine();
-        System.out.println(" Hello! I'm Pain");
-        System.out.println(" What can I do for you?");
+        System.out.println("    Nihao! I'm Pain");
+        System.out.println("    Yo want you want");
         printLine();
-        System.out.println(" Bye. Hope to see you again soon!");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        printLine();
+        while(!input.equals("bye")) {
+            System.out.println("    " + input);
+            printLine();
+            input = sc.nextLine();
+            printLine();
+        }
+        System.out.println("    Bye. I will definitely see you again");
         printLine();
     }
 }
