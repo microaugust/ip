1ff46e4c6ccf18a25f7b1b089ab28c33ecbe2abb
microaugust <203528273+microaugust@users.noreply.github.com>
2026-01-22 12:21:33 +0800

    Level-0

---
+++ src/main/java/Pain.java
@@ -0,0 +1,15 @@
+public class Pain {
+
+    private static void printLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
+        printLine();
+        System.out.println(" Hello! I'm Pain");
+        System.out.println(" What can I do for you?");
+        printLine();
+        System.out.println(" Bye. Hope to see you again soon!");
+        printLine();
+    }
+}
