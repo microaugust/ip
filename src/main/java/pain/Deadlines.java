6fd23fc6fdac4225c951871e2cd338417b8da4c1
microaugust <203528273+microaugust@users.noreply.github.com>
2026-01-29 18:20:13 +0800

    Added JavaDoc

--- src/main/java/pain/Deadlines.java
+++ src/main/java/pain/Deadlines.java
@@ -9,21 +9,42 @@ public class Deadlines extends Task{
 
     private LocalDateTime by;
     
+    /**
+     * Return a new Deadlines Object.
+     * isDone will be false by default.
+     * 
+     * @param task Name of the Deadlines task.
+     * @param by Deadlines time in "dd/mm/yy hh:mm:ss" format.
+     * @return Deadlines object.
+     */
     public Deadlines(String task, String by) {
         super(task);
         this.by = LocalDateTime.parse(by, inputFormatter);
     }
 
+    /**
+     * Return a new Deadlines Object.
+     * 
+     * @param task Name of the Deadlines task.
+     * @param isDone Mark the Deadlines being done as true or false.
+     * @param by Deadlines date in "dd/mm/yy hh:mm:ss" format.
+     * @return Deadlines object.
+     */
     public Deadlines(String task, boolean isDone, String by) {
         super(task, isDone);
         this.by = LocalDateTime.parse(by, inputFormatter);
     }
 
-    public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.by.format(outputFormatter) + ")";
-    }
-
+    /**
+     * Return the String format of how this task is saved in the hard disk.
+     * 
+     * @return String format of how this task is saved in the hard disk.
+     */
     public String saveText() {
         return "Deadline | " + (this.getIsDone() ? "1" : "0") + " | " + this.getTaskName() + " | " + this.by.format(inputFormatter);
+    }
+
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + this.by.format(outputFormatter) + ")";
     }
 }
--- src/main/java/pain/ToDos.java
+++ src/main/java/pain/ToDos.java
@@ -12,6 +12,11 @@ public class ToDos extends Task{
         return "[T]" + super.toString();
     }
 
+    /**
+     * Return the String format of how this task is saved in the hard disk.
+     * 
+     * @return String format of how this task is saved in the hard disk.
+     */
     public String saveText() {
         return "ToDo | " + (this.getIsDone() ? "1" : "0") + " | " + this.getTaskName();
     }
