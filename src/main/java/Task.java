f6cb97496e601343854f45f32fb4bc0af3037c24
microaugust <203528273+microaugust@users.noreply.github.com>
2026-01-29 15:54:36 +0800

    Prepackage

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -22,8 +22,6 @@ public class Task{
      */
     public void mark() {
         this.isDone = true;
-        System.out.println("    Nice! I've marked this task as done:");
-        System.out.println("      " + this.toString());
     }
 
     /**
@@ -31,8 +29,6 @@ public class Task{
      */
     public void unmark() {
         this.isDone = false;
-        System.out.println("    OK, I've marked this task as not done yet:");
-        System.out.println("      " + this.toString());
     }
 
     public String saveText() {
