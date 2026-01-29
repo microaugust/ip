0ad811fb0c9cf159d6faff4d9173c62f43c2b0aa
microaugust <203528273+microaugust@users.noreply.github.com>
2026-01-29 14:51:29 +0800

    A-MoreOOP

--- src/main/java/Pain.java
+++ src/main/java/Pain.java
@@ -8,18 +8,17 @@ import java.time.format.DateTimeParseException;
 public class Pain{
 
     static final String PATHNAME = "data/pain.txt";
+    static final String NAME = "Pain";
 
     static Scanner sc = new Scanner(System.in);
-
-    private static void printLine() {
-        System.out.println("    ____________________________________________________________");
-    }
 
     public static void main(String[] args) throws IOException{
         File data = new File("data");
         File dataText = new File(PATHNAME);
         Storage taskStorage = new Storage(PATHNAME);
         Parser parser = new Parser();
+        Ui ui = new Ui(NAME);
+
         if(!data.exists()) {
             data.mkdirs();
         }
@@ -30,24 +29,19 @@ public class Pain{
             taskList = new TaskList();
         }
 
-        printLine();
-        System.out.println("    Nihao! I'm Pain");
-        System.out.println("    Yo want you want");
-        printLine();
+        ui.startUp();
+
         while(true) {
             try{
-                String input = sc.nextLine();
-                printLine();
-                String[] parsedInput = parser.parseInput(input);
+                String[] parsedInput = ui.getAndParseInput(sc, parser);
                 switch(parsedInput[0]) {
                 case "bye":
-                    System.out.println("    Bye. I will definitely see you again");
-                    printLine();
+                    ui.printOutput(parsedInput, taskList);
                     sc.close();
                     System.exit(0);
 
                 case "list":
-                    System.out.println(taskList.toString());
+                    ui.printOutput(parsedInput, taskList);
                     break;
 
                 case "mark":
@@ -56,6 +50,7 @@ public class Pain{
                         throw new NotInListException();
                     }
                     taskList.get(taskToMark).mark();
+                    ui.printOutput(parsedInput, taskList);
                     taskStorage.saveTaskOnHardDisk(taskList);
                     break;
 
@@ -65,45 +60,38 @@ public class Pain{
                         throw new NotInListException();
                     }
                     taskList.get(taskToUnmark).unmark();
+                    ui.printOutput(parsedInput, taskList);
                     taskStorage.saveTaskOnHardDisk(taskList);
                     break;
 
                 case "todo":
                     Task todoTask = new ToDos(parsedInput[1]);
                     taskList.add(todoTask);
-                    System.out.println("    Got it. I've added this task:");
-                    System.out.println("      " + todoTask.toString());
-                    System.out.println("    Now you have " + taskList.size() + " tasks in the list.");
+                    ui.printOutput(parsedInput, taskList);                    
                     taskStorage.saveTaskOnHardDisk(taskList);
                     break;
 
                 case "deadline":
                     Task deadlineTask = new Deadlines(parsedInput[1], parsedInput[2]);
                     taskList.add(deadlineTask);
-                    System.out.println("    Got it. I've added this task:");
-                    System.out.println("      " + deadlineTask.toString());
-                    System.out.println("    Now you have " + taskList.size() + " tasks in the list.");
+                    ui.printOutput(parsedInput, taskList);
                     taskStorage.saveTaskOnHardDisk(taskList);
                     break;
 
                 case "event":
                     Task eventTask = new Events(parsedInput[1], parsedInput[2], parsedInput[3]);
                     taskList.add(eventTask);
-                    System.out.println("    Got it. I've added this task:");
-                    System.out.println("      " + eventTask.toString());
-                    System.out.println("    Now you have " + taskList.size() + " tasks in the list.");
+                    ui.printOutput(parsedInput, taskList);
                     taskStorage.saveTaskOnHardDisk(taskList);
                     break;
 
                 case "delete":
-                    int taskToDelete = Integer.parseInt(parsedInput[1]);
+                    int taskToDelete = Integer.parseInt(parsedInput[1]) - 1;
                     if(taskToDelete >= taskList.size()) {
                         throw new NotInListException();
                     }
-                    System.out.println("    Noted. I've removed this task:");
-                    System.out.println("      " + taskList.get(taskToDelete).toString());
-                    taskList.delete(taskToDelete);
-                    System.out.println("    Now you have " + taskList.size() + " tasks in the list.");
+                    ui.printOutput(parsedInput, taskList);
+                    taskList.delete(taskToDelete);                    
                     taskStorage.saveTaskOnHardDisk(taskList);
                     break;
 
@@ -112,18 +100,22 @@ public class Pain{
                 }
             } catch (NoCommandException e) {
                 System.out.println("    brotha, what are you typing? i don't understand u");
+                ui.printLine();
             } catch (EmptyCommandException e) {
                 System.out.println("    ooooooh, this cannot be empty lah");
+                ui.printLine();
             } catch (InvalidCommandException e) {
                 System.out.println("    aiya recheck whether this correct format or not aah");
+                ui.printLine();
             } catch (NotInListException e) {
                 System.out.println("    that index not in the list lol");
+                ui.printLine();
             } catch (NumberFormatException e) {
                 System.out.println("    need to be a number");
+                ui.printLine();
             } catch (DateTimeParseException e) {
                 System.out.println("    invalid date time format (need to be dd/mm/yyyy hh:mm:ss)");
-            } finally {
-                printLine();
+                ui.printLine();
             }
         }
     }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Ui {
+
+    private String name;
+
+    public Ui(String name) {
+        this.name = name;
+    }
+
+    public void printLine() {
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public void startUp() {
+        printLine();
+        System.out.println("    Nihao! I'm " + this.name);
+        System.out.println("    Yo want you want");
+        printLine();
+    }
+
+    public String[] getAndParseInput(Scanner sc, Parser p) throws InvalidCommandException, EmptyCommandException, NoCommandException{
+        String input = sc.nextLine();
+        printLine();
+        String[] parsedInput = p.parseInput(input);
+        return parsedInput;
+    }
+
+    public void printOutput(String[] parseInput, TaskList taskList) throws NoCommandException{
+        switch(parseInput[0]) {
+        case "bye":
+            System.out.println("    Bye. I will definitely see you again");
+            break;
+        case "list":
+            System.out.println(taskList.toString());
+            break;
+        case "mark":
+            int markIndex = Integer.parseInt(parseInput[1]) - 1;
+            System.out.println("    Nice! I've marked this task as done:");
+            System.out.println("      " + taskList.get(markIndex).toString());
+            break;
+        case "unmark":
+            int unmarkIndex = Integer.parseInt(parseInput[1]) - 1;
+            System.out.println("    OK, I've marked this task as not done yet:");
+            System.out.println("      " + taskList.get(unmarkIndex).toString());
+            break;
+        case "todo":
+        case "deadline":
+        case "event":
+            System.out.println("    Got it. I've added this task:");
+            System.out.println("      " + taskList.get(taskList.size() - 1).toString());
+            System.out.println("    Now you have " + taskList.size() + " tasks in the list.");
+            break;
+        case "delete":
+            int deleteIndex = Integer.parseInt(parseInput[1]) - 1;
+            System.out.println("    Noted. I've removed this task:");
+            System.out.println("      " + taskList.get(deleteIndex).toString());
+            System.out.println("    Now you have " + (taskList.size() - 1) + " tasks in the list.");
+            break;
+        default:
+            throw new NoCommandException();
+        }
+        printLine();
+    }
+}
